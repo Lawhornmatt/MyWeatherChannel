d8406fa109be07def89bb25e7ee481e8b8252b20
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-08-01 04:51:53 -0500

    Data Access & Storage
    Upon user input, location name is converted into lat and longitudes, which are then fed into the main OneCall openweatherAPI to get the weather conditions. All location weather data is accessible. A button is generated with the lat and long stored as respective dataset.

--- libs/main.js
+++ libs/main.js
@@ -9,7 +9,7 @@
 // ====================
 
 const searchBtn = document.getElementById('searchBtn'); //the Button to click to make a search
-const citySearch = document.getElementById('citySearch'); //the input box ppl type their search into
+const locSearch = document.getElementById('locSearch'); //the input box ppl type their search into
 const hisCon = document.getElementById('historyContainer'); //append gen'd History Buttons here
 const cityBox = document.getElementById('cityBox'); //displays the current weather of the city
 const gibFCCs = document.getElementById('gibFCCs'); //append forecast weather cards to this container
@@ -19,7 +19,18 @@ const gibFCCs = document.getElementById('gibFCCs'); //append forecast weather ca
 //   INITIALIZATIONS
 // ====================
 
-const APIurl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly,daily,alerts&appid=3b3319e2a4bdc403d7f45843c07de674';
+var locationName;
+
+// var currentLAT;
+// var currentLON;
+
+// var currentLAT = String(29.7589382);
+// var currentLON = String(-95.3676974);
+
+// currentLAT = currentLAT.substring(0, 5);
+// console.log('LATitude is: ' + currentLAT);
+// currentLON = currentLON.substring(0, 5);
+// console.log('LONgitude is: ' + currentLON);
 
 // ====================
 //      FUNCTIONS
@@ -28,66 +39,112 @@ const APIurl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-9
 //Gets called by clicking the button. 
 //Gathers the input'd info, sanitizes it, then
 //Bifurcates and passes info into //History and into //getAPI
-function btnGO(e) {
-    // e.preventDefault();
+function btnGO() {
 
     //Sanitize search inputs
-    var cityName = String(citySearch.value);
-    if (cityName == "") {
+    locationName = String(locSearch.value);
+    if (locationName == "") {
         return;
-    } else if (cityName == undefined) {
+    } else if (locationName == undefined) {
         return console.log('Input was undefined');
-    } else if (/\d/.test(cityName)) {
+    } else if (/\d/.test(locationName)) {
         return console.log('No Numbers Please');
-    } else if (/(?!,)(?! )\W|_/.test(cityName)) {
+    } else if (/(?!,)(?! )\W|_/.test(locationName)) {
         return console.log('No Odd Characters Please');
     } else {
-        console.log('You searched for: ' + cityName);
+        console.log('You searched for: ' + locationName);
     }
     
+    var LONGLATurl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + locationName + '&limit=1&appid=3b3319e2a4bdc403d7f45843c07de674';
+
     // generate a History Button
-    genHB(cityName);
+    // genHB(locationName);
 
     //fetch from the Weather API
-    fetchAPI();
+
+    // const geoData = fetch(LONGLATurl)
+
+    // .then(function (response) {
+    //   return response.json();
+    // })
+
+    // .then((data) => {
+    //   return [data[0].lat, data[0].lon];
+    // });
+
+    // const giveLongLats = () => {
+    //     geoData.then((a) => {
+    //         console.log(a);
+    //         });
+    // };
+
+    // giveLongLats();
+
+    geoAPI(LONGLATurl);
+
+
+
+    // let locationLAT = locaGEO.then(value => value[0].lat);
+    // let locationLON = locaGEO.then(value => value[0].lon);
+    
+    // mainAPI(locationLAT, locationLON);
+    // var locationData = mainAPI(locationLAT, locationLON);
 };
 
 //Generates a History Button
-function genHB(cityName) {
+function genHB(locationName, Lat, Lon) {
 
     let freshBtn = document.createElement('button');
     freshBtn.type = 'button';
     freshBtn.classList.add('btn', 'btn-light', 'btn-block', 'btntizeMe');
-    freshBtn.innerHTML = cityName;
+    freshBtn.innerHTML = locationName;
 
-    //freshBtn.addEventListener("click", fetchAPI); //Just goes straight to fetchAPI and skips all the other stuff
+    freshBtn.dataset.lat = Lat;
+    freshBtn.dataset.lon = Lon;
+
+    //freshBtn.addEventListener("click", MAKESOMETHINGNEW); //Just goes straight to fetchAPI and skips all the other stuff
 
     hisCon.appendChild(freshBtn);
 };
 
 
-//Uses the initialized APIurl to make a request for data
-//TODO: make initialized APIurl change-able with input
-function fetchAPI() {
+//First, we use openweather's Geocoding API to get the needed Lat and Long from the user input
+function geoAPI(url) {
 
-  fetch(APIurl)
+    const geoData = fetch(url)
 
     .then(function (response) {
       return response.json();
     })
 
-    .then(function (data) {
-      console.log(data);
-    //   for (var i = 0; i < data.length; i++) {
-    //     var userName = document.createElement('h3');
-    //     var issueTitle = document.createElement('p');
-    //     userName.textContent = data[i].user.login;
-    //     issueTitle.textContent = data[i].title;
-    //     issueContainer.append(userName);
-    //     issueContainer.append(issueTitle);
-    //   }
-        console.log(data.current.temp);
+    .then((data) => {
+      return [data[0].lat, data[0].lon];
     });
+
+    const giveLongLats = () => {
+        geoData.then((a) => {
+            mainAPI(a[0], a[1]);
+            });
+    };
+
+    giveLongLats();
+};
+
+//We then pass the those Lat and Long into the main openweather API 
+function mainAPI(Lat, Lon) {
+
+    var TRUEurl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+Lat+'&lon='+Lon+'&exclude=minutely,hourly,daily,alerts&appid=3b3319e2a4bdc403d7f45843c07de674';
+
+    fetch(TRUEurl)
+
+      .then(function (response) {
+        return response.json();
+      })
+
+      .then(function (data) {
+          console.log(data);
+          genHB(locationName, Lat, Lon);
+      });
 };
 
 //Adds event listener to search button
