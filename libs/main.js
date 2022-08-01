77f410ac5c1ee7e2cc8064bdd6c87fb0799911c5
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-07-31 19:00:47 -0500

    Basic DOM Attch
    Can access the btn and attempt a fetch

--- libs/main.js
+++ libs/main.js
@@ -8,18 +8,42 @@
 //  ACCESS HTML BY DOM
 // ====================
 
-// var YADA = document.getElementById("YADA");
-// var YADA = $('.yada');
+const fetchBtn = $('#fetchBtn');
+const cityBox = $('#cityBox');
+const gibFCCs = $('#gibFCCs');
+
 
 // ====================
 //   INITIALIZATIONS
 // ====================
 
+const APIurl = 'https://api.github.com/repos/IBM/clai/issues?per_page=5';
+
+
 // ====================
 //      FUNCTIONS
 // ====================
 
+function getAPI() {
 
+  fetch(requestUrl)
 
+    .then(function (response) {
+      return response.json();
+    })
 
+    .then(function (data) {
+      console.log(data);
+    //   for (var i = 0; i < data.length; i++) {
+    //     var userName = document.createElement('h3');
+    //     var issueTitle = document.createElement('p');
+    //     userName.textContent = data[i].user.login;
+    //     issueTitle.textContent = data[i].title;
+    //     issueContainer.append(userName);
+    //     issueContainer.append(issueTitle);
+    //   }
+    });
+}
+
+fetchBtn.on('click', getAPI);
 
